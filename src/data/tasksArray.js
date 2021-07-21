2430a042728af32a25e590f9fdb9a55509314350
grassleaf99 <65818460+grassleaf99@users.noreply.github.com>
2021-07-21 17:11:08 +0700

    update init data

--- src/data/tasksArray.js
+++ src/data/tasksArray.js
@@ -2,19 +2,19 @@ const arrayTasks = [
   {
     id: 1,
     content: "Learn ReactJS",
-    time: "9/2/2021",
+    time: "2021-02-09",
     complete: false,
   },
   {
     id: 2,
     content: "Learn React Native",
-    time: "20/7/2021",
+    time: "2021-07-20",
     complete: false,
   },
   {
     id: 3,
     content: "Learn NodeJS",
-    time: "9/2/2021",
+    time: "2021-09-02",
     complete: false,
   },
 ];
